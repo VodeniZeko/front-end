c3bc2ad89a261a7d87577ff4b8dde0587e1765f3
Jose <Jrivera6869@yahoo.com>
2020-01-31 19:14:41 -0500

    test

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <h1>Use My Tech Stuff</h1>
+        <p>Hello</p>
         <a
           className='App-link'
           href='https://reactjs.org'
