2b940b2b8dac03def60a0fb588789b0d21371921
VodeniZeko <salexy.es@gmail.com>
2020-02-02 14:52:23 -0800

    main page styles

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import image from "../assets/techItems.jpg";
 
 export const NavbarContainer = styled.div`
   display: flex;
@@ -131,11 +132,11 @@ export const NavbarContainer = styled.div`
 export const MainPageContainer = styled.div`
   display: flex;
   flex-direction: column;
-  margin:5em;
   min-width:400px;
   .headerContainer {
     text-align: center;
     display: flex;
+    margin:5em;
     flex-direction: column;
     justify-content: center;
     height: 45vh;
@@ -191,13 +192,199 @@ export const MainPageContainer = styled.div`
     background-size: 100%;
     transition: background 0s;
     }
+
     .headerContainer > button:focus{
       outline:0;
+    }
+    .imageContainer {
+      background-image:url(${image});
+      height:40vh;
+      background-size:cover;
+
     }
     .imageContainer > img {
       width:100%;
       padding:3em 0em;
-      border-radius:40em;
+    }
+
+   
+    .contentContainer {
+      margin-bottom:2em;
+    }
+    #conference-timeline {
+      position: relative;
+      max-width: 920px;
+      width: 100%;
+      margin: 0 auto;
+      padding-top:5em;
+    }
+    #conference-timeline .timeline-start,
+    #conference-timeline .timeline-end {
+      display: table;
+      font-family: 'Nixie One', cursive;
+      font-size: 18px;
+      font-weight: 900;
+      text-transform: uppercase;
+      background: #1d9af2;
+      padding: 15px 23px;
+      color: #fff;
+      max-width: 5%;
+      width: 100%;
+      text-align: center;
+      margin: 0 auto;
+    }
+    #conference-timeline .conference-center-line {
+      position: absolute;
+      width: 3px;
+      height: 100%;
+      top: 0;
+      left: 50%;
+      margin-left: -2px;
+      background: #00b0bd;
+      z-index: -1;
+    }
+    #conference-timeline .conference-timeline-content {
+      padding-top: 67px;
+      padding-bottom: 67px;
+    }
+    .timeline-article {
+      width: 100%;
+      height: 100%;
+      position: relative;
+      overflow: hidden;
+      margin: 20px 0;
+    }
+    .timeline-article .content-left-container,
+    .timeline-article .content-right-container {
+      max-width: 44%;
+      width: 100%;
+    }
+    .timeline-article .content-left,
+    .timeline-article .content-right {
+      position: relative;
+      width: auto;
+      border: 1px solid #ddd;
+      background-color: #fff;
+      box-shadow: 0 1px 3px rgba(0,0,0,.03);
+      padding: 27px 25px;
+    }
+    .timeline-article p {
+      margin: 0 0 0 60px;
+      padding: 0;
+      font-weight: 400;
+      color: #242424;
+      font-size: 14px;
+      line-height: 24px;
+      position: relative;
+      font-family: 'Nixie One', cursive;
+      font-weight:bold;
+    }
+    .timeline-article p span.article-number {
+      position: absolute;
+      font-weight: 300;
+      font-size: 44px;
+      top: 10px;
+      left: -60px;
+      color: #00b0bd;
+    }
+    .timeline-article .content-left-container {
+      float: left;
+    }
+    .timeline-article .content-right-container {
+      float: right;
+    }
+    .timeline-article .content-left:before,
+    .timeline-article .content-right:before{
+      position: absolute;
+      top: 20px;
+      font-size: 23px;
+      font-family: 'Nixie One', cursive;
+      color: #fff;
+    }
+    .timeline-article .content-left:before {
+      content: "\f0da";
+      right: -8px;
+    }
+    .timeline-article .content-right:before {
+      content: "\f0d9";
+      left: -8px;
+    }
+    .timeline-article .meta-date {
+      position: absolute;
+      top: 0;
+      left: 50%;
+      width: 72px;
+      height: 72px;
+      margin-left: -31px;
+      color: #fff;
+      border-radius: 100%;
+      background: #1d9af2;
+    }
+    .timeline-article .meta-date .date,
+    .timeline-article .meta-date .month {
+      display: block;
+      text-align: center;
+      font-weight: 900;
+      margin: 10px;
+    }
+    .timeline-article .meta-date .date {
+      font-size: 18px;
+      
+    }
+    .timeline-article .meta-date .month {
+      font-size: 18px;
+      line-height: 10px;
+    }
+
+    /*===== // Vertical Timeline =====*/
+    
+    @media only screen and (max-width: 830px) {
+      .contentContainer #conference-timeline .timeline-start,
+      #conference-timeline .timeline-end {
+        margin: 0;
+      }
+      .contentContainer #conference-timeline .conference-center-line {
+        margin-left: 0;
+        left: 50px;
+        
+      }
+      .contentContainer .timeline-article .meta-date {
+        margin-left: 0;
+        left: 20px;
+      }
+      .contentContainer .timeline-article .content-left-container,
+      .contentContainer .timeline-article .content-right-container {
+        max-width: 100%;
+        width: auto;
+        float: none;
+        margin-left: 110px;
+        min-height: 53px;
+      }
+      .contentContainer .timeline-article .content-left-container {
+        margin-bottom: 20px;
+      }
+      .contentContainer .timeline-article .content-left,
+      .contentContainer .timeline-article .content-right {
+        padding: 10px 25px;
+        min-height: 65px;
+      }
+      .contentContainer .timeline-article .content-left:before {
+        content: "\f0d9";
+        right: auto;
+        left: -8px;
+      }
+      .contentContainer .timeline-article .content-right:before {
+        display: none;
+      }
+    }
+    @media only screen and (max-width: 400px) {
+      .contentContainer .timeline-article p {
+        margin: 0;
+      }
+      .contentContainer .timeline-article p span.article-number {
+        display: none;
+      }
+      
     }
 `;
 
