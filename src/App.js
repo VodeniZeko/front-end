180549aed3cff25f57ca233fbbda42f3b7255366
VodeniZeko <salexy.es@gmail.com>
2020-02-03 13:10:00 -0800

    modal

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Login from "./components/Login";
-import SignUp from "./components/SignUp";
+import LoginModal from "./components/LoginModal";
+import SignUpModal from "./components/SignUpModal";
 import Lost from "./components/Lost";
 import MainPage from "./components/MainPage";
 import Profile from "./components/Profile";
@@ -17,10 +17,10 @@ export default function App() {
           <MainPage />
         </Route>
         <Route path="/login">
-          <Login />
+          <LoginModal />
         </Route>
         <Route path="/signup">
-          <SignUp />
+          <SignUpModal />
         </Route>
         <Route path="/profile">
           <Profile />
---
+++ src/components/SignUpModal.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { Modal, Button } from "react-bootstrap";
+import SignUp from "./SignUp";
+
+const LoginModal = () => {
+  const [show, setShow] = useState(true);
+
+  return (
+    <div>
+      <Modal
+        show={show}
+        onHide={() => setShow(false)}
+        dialogClassName="modal-90w"
+        centered="true"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="SignUp">Welcome to Shareable</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <SignUp />
+          <Modal.Footer>
+            <span>Already have an account?</span>
+            <Button variant="outline-secondary">Login!</Button>
+          </Modal.Footer>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+};
+
+export default LoginModal;
