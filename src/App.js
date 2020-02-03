fe9d8877971bcad11832474cd96386c8ddf97d8f
Jose <Jrivera6869@yahoo.com>
2020-02-03 17:32:55 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -14,23 +14,23 @@ export default function App() {
     <main>
       <Navbar />
       <Switch>
-        {/* <Route path="/EquipmentUpload">
+        <Route path='/EquipmentUpload'>
           <EquipmentUploadForm />
-        </Route> */}
-        <Route exact path="/">
+        </Route>
+        <Route exact path='/'>
           <MainPage />
         </Route>
-        <Route path="/login">
+        <Route path='/login'>
           <LoginModal />
         </Route>
-        <Route path="/signup">
+        <Route path='/signup'>
           <SignUpModal />
         </Route>
         {/* <Route path=''> */}
-        <Route path="/profile">
+        <Route path='/profile'>
           <Profile />
         </Route>
-        <Route path="">
+        <Route path=''>
           <Lost />
         </Route>
       </Switch>
