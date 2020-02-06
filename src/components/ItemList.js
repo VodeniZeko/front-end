718d2153dcc1097f65272d328a5a5093c14afde2
Jose <Jrivera6869@yahoo.com>
2020-02-06 18:51:56 -0500

    fixed again

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -50,7 +50,7 @@ const ItemList = () => {
       />
       <Scroll>
         <ItemContainer>
-          {items
+          {filtered
             // .filter(item => item.availability !== 0) // SHOWS ONLY AVAILABLE PRODUCTS
             .map(item => (
               <div class='container'>
