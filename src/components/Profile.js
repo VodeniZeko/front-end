eb2a4d0bc823da0e427508a9a207c787b6e8707a
VodeniZeko <salexy.es@gmail.com>
2020-02-05 16:45:40 -0800

    profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { ListGroup } from "react-bootstrap";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
 import axios from "axios";
 import { ProfileContainer } from "../styles/styles.js";
+import Stars from "./stars";
 import profile from "../assets/profile.svg";
 import { useSelector, useDispatch } from "react-redux";
 import { Fetch } from "../actions/Apicalls.js";
-import ProfileItems from "./ProfileItems.js";
+// import ProfileItems from "./ProfileItems.js";
 import { NavLink } from "react-router-dom";
 
 const Profile = props => {
+  const a = "	\u25BC";
   var check = "	\u2713";
   var noCheck = "	\u274C";
   const [user, setUser] = useState([]);
@@ -38,11 +40,14 @@ const Profile = props => {
       <div>
         <img style={{ top: "480px" }} id="icon" src={user.image} alt="" />
       </div>
-      <div className="listContainer" style={{ display: "flex" }}>
+      <div
+        className="listContainer"
+        style={{ display: "flex", margin: "0 auto" }}
+      >
         <ListGroup
           variant="flush"
           style={{
-            padding: "2em 5em",
+            padding: "1em 5em 0em 0em",
             fontSize: "2.5rem",
             height: "50vh",
             border: "none"
@@ -51,7 +56,7 @@ const Profile = props => {
           <ListGroup.Item
             style={{ border: "none", fontFamily: "'Nixie One', cursive" }}
           >
-            Verified info
+            Verified info {a}
           </ListGroup.Item>
           {console.log(props, "props")}
           <ListGroup.Item style={{ border: "none" }}>
@@ -72,7 +77,7 @@ const Profile = props => {
         <ListGroup
           variant="flush"
           style={{
-            padding: "2em 5em",
+            padding: "1em 0em",
             fontSize: "2.5rem",
             height: "50vh",
             border: "none"
@@ -81,13 +86,12 @@ const Profile = props => {
           <ListGroup.Item
             style={{ border: "none", fontFamily: "'Nixie One', cursive" }}
           >
-            Your Items !
+            Your Items{a}
           </ListGroup.Item>
 
           <ListGroup.Item style={{ border: "none" }}>
             {items
               .filter(stuff => stuff.user_id === currentuser.id)
-
               .map(item => (
                 <ListGroup.Item
                   style={{
@@ -95,15 +99,47 @@ const Profile = props => {
                     fontFamily: "'Nixie One', cursive"
                   }}
                 >
-                  <NavLink to="/PersonalItems">{item.item_name}</NavLink>
+                  <div
+                    style={{
+                      width: "50px",
+                      height: "50px"
+                    }}
+                  >
+                    <NavLink to="/PersonalItems">
+                      {" "}
+                      <img src={item.imgs} />
+                    </NavLink>
+                  </div>
                 </ListGroup.Item>
               ))}
 
             {console.log(items, "items")}
           </ListGroup.Item>
-          <ListGroup.Item style={{ border: "none" }}></ListGroup.Item>
-          <ListGroup.Item style={{ border: "none" }}></ListGroup.Item>
-          <ListGroup.Item style={{ border: "none" }}></ListGroup.Item>
+        </ListGroup>
+        <ListGroup
+          variant="flush"
+          style={{
+            padding: "1em 5em",
+            fontSize: "2.5rem",
+            height: "50vh",
+            border: "none"
+          }}
+        >
+          <ListGroup.Item
+            style={{ border: "none", fontFamily: "'Nixie One', cursive" }}
+          >
+            Reviews from Owners{a}
+          </ListGroup.Item>
+          <ListGroup.Item style={{ border: "none" }}>
+            <img src="https://thumbs.dreamstime.com/t/creative-vector-illustration-default-avatar-profile-placeholder-isolated-background-art-design-grey-photo-blank-template-mo-118823351.jpg" />
+          </ListGroup.Item>
+          <ListGroup.Item style={{ border: "none" }}>
+            <Stars />
+            <p>No reviews yet</p>
+            <span style={{ opacity: ".8" }}>
+              unsername hasn't rented anything yet
+            </span>
+          </ListGroup.Item>
         </ListGroup>
       </div>
     </ProfileContainer>
